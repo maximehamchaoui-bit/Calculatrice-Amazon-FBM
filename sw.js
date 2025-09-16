98d73e39a937cb9b6d77d60d37f5ef7e11b001b3
maximehamchaoui-bit <maximehamchaoui@gmail.com>
2025-09-17 00:08:15 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-const CACHE = 'fbm-cache-v1-2025-09-16T21:38:31.277592';
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'])));
-});
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request)));
-});
